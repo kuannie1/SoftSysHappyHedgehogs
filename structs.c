2e61c5cc63d29b164c92c6a99dff8f4006204183
kuannie1 <anne.ku4@gmail.com>
2017-02-21 23:59:12 -0500

    updated struct file

--- structs.c
+++ structs.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct http_message {
-	request request_line;
-	response response_line;
-} http_message;
-
-typedef struct generic_message {
-	char * startline;
-	messageheader message_header;
-	crlf CRLF;
-	char * message_body;
-} message;
+typedef enum {
+	HEAD, POST, GET, PUT, DELETE, OPTIONS, CONNECT
+} request_type;
 
 typedef struct messageheader {
 	char * field_name;
-	fieldvalue field_value;
+	char * field_value; //is field_content
 } messageheader;
 
-// I think I still need a struct for field value and everything after that.
+typedef struct req_startline {
+	request_type req_type;
+	char * url;
+	char * http_ver;
+} req_startline;
 
-typedef struct startline {
-	request request_line;
-	response response_line;
-} startline;
+typedef struct status {
+	int code;
+	char * text;
+} status;
 
-typedef struct request {
+typedef struct res_startline {
+	char * http_ver;
+	status status_code;
+} res_startline;
 
-} request;
 
 typedef struct response {
-
+	res_startline startline;
+	messageheader headers[101];
 } response;
+
+typedef struct request {
+	req_startline startline;
+	messageheader headers[101];
+} request;
+
+int main() {
+	request req = {{GET, "/api/testing", "HTTP/1.1"}};
+	req.headers[0] = (messageheader) {"Cookie", "chocolate chip"};
+	req.headers[1] = (messageheader) {"Accept", "*/*"};
+
+	response res = {{"HTTP/1.1", {200, "OK"}}};
+	res.headers[0] = (messageheader) {"Content-Type","text/html"};
+	res.headers[1] = (messageheader) {"Server","dankwebsite.com"};
+	return 0;
+}
