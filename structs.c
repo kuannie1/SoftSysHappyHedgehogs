50158df230ea6a6bdb4ef0010543d19479de3460
kuannie1 <anne.ku4@gmail.com>
2017-02-22 01:42:46 -0500

    update with regex implementation

--- structs.c
+++ structs.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
+
 
 typedef enum {
 	HEAD, POST, GET, PUT, DELETE, OPTIONS, CONNECT
@@ -47,5 +49,43 @@ int main() {
 	res.headers[0] = (messageheader) {"Content-Type","text/html"};
 	res.headers[1] = (messageheader) {"Server","dankwebsite.com"};
 	res.body = "<h1>Hello World!</h1>";
+
+	char * raw_req = "GET /api/testing tester HTTP/1.1\r\nCookie:chocolate chip\r\nAccept:*/*";
+	// Parse Req
+	regex_t regex;
+	int reti;
+	int i;
+	int j;
+	size_t nmatch = 2;
+	// regmatch_t pmatch[2];
+	regmatch_t* pmatch = malloc(sizeof(regex_t) * (regex.re_nsub + 1));
+
+	regcomp(&regex, "\\(.*?\r\n\\)", 0);
+	// regcomp(&regex, "\\w*:\\w*", 0);
+	// regcomp(&regex, "\\(test\\)(?:ing)", 0);
+	// reti = regexec(&regex, raw_req, nmatch, pmatch, 0);
+	// printf("a matched substring \"%.*s\" is found at position %d to %d.\n",       
+	//      pmatch[1].rm_eo - pmatch[1].rm_so, &raw_req[pmatch[1].rm_so],  
+	//      pmatch[1].rm_so, pmatch[1].rm_eo - 1);
+
+	if ((reti = regexec(&regex, raw_req, regex.re_nsub + 1, pmatch, 0))){
+	        printf("no match\n");
+	        /* error or no match */
+	    } else {
+	    	printf("match!\n");
+	    	// printf("a matched substring \"%.*s\" is found at position %d to %d.\n",       
+	    	//      pmatch[1].rm_eo - pmatch[1].rm_so, &raw_req[pmatch[1].rm_so],  
+	    	//      pmatch[1].rm_so, pmatch[1].rm_eo - 1);
+	        for(i = 0; i < regex.re_nsub; i++) {
+	            printf("match %d from index %d to %d: ", i, pmatch[i].rm_so,
+	                   pmatch[i].rm_eo);
+	            for(j = pmatch[i].rm_so; j < pmatch[i].rm_eo; j++) {
+	                printf("%c", raw_req[j]);
+	            }
+	            printf("\n");
+	        }
+	    }
+	free(pmatch);
+	regfree(&regex);
 	return 0;
 }
