c5007750931175e146cf4b9ad3f797a10d4c4308
Serena Chen <serenazychen@gmail.com>
2017-02-23 11:46:51 -0500

    moved structs to header file

--- library/structs.c
+++ library/structs.c
@@ -1,45 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <regex.h>
-
-#define MAX_HEADER_SIZE 101
-
-typedef enum {
-	HEAD, POST, GET, PUT, DELETE, OPTIONS, CONNECT
-} request_type;
-
-typedef struct {
-	char * field_name;
-	char * field_value; //is field_content
-} MessageHeader;
-
-typedef struct {
-	request_type req_type;
-	char * url;
-	char * http_ver;
-} RequestLine;
-
-typedef struct {
-	int code;
-	char * reason_phrase;
-} Status;
-
-typedef struct {
-	char * http_ver;
-	Status status_code;
-} StatusLine;
-
-typedef struct {
-	StatusLine status_line;
-	MessageHeader headers[MAX_HEADER_SIZE];
-	char * body;
-} Response;
-
-typedef struct {
-	RequestLine request_line;
-	MessageHeader headers[MAX_HEADER_SIZE];
-	char * body;
-} Request;
+#include "structs.h"
 
 int main() {
 	Request req = {{POST, "/api/helloworld", "HTTP/1.1"}};
---
+++ library/structs.h
@@ -0,0 +1,43 @@
+#ifndef _structs_h
+#define _structs_h
+
+#define MAX_HEADER_SIZE 101
+
+typedef enum {
+	HEAD, POST, GET, PUT, DELETE, OPTIONS, CONNECT
+} request_type;
+
+typedef struct {
+	char * field_name;
+	char * field_value; //is field_content
+} MessageHeader;
+
+typedef struct {
+	request_type req_type;
+	char * url;
+	char * http_ver;
+} RequestLine;
+
+typedef struct {
+	int code;
+	char * reason_phrase;
+} Status;
+
+typedef struct {
+	char * http_ver;
+	Status status_code;
+} StatusLine;
+
+typedef struct {
+	StatusLine status_line;
+	MessageHeader headers[MAX_HEADER_SIZE];
+	char * body;
+} Response;
+
+typedef struct {
+	RequestLine request_line;
+	MessageHeader headers[MAX_HEADER_SIZE];
+	char * body;
+} Request;
+
+#endif
