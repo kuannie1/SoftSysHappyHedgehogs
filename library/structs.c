f7c02fb41365ade0c486c32459dc2308b55bcd25
billmwong <billmwong@gmail.com>
2017-02-24 18:21:03 -0500

    wip function to get regex matches

--- library/structs.c
+++ library/structs.c
@@ -6,32 +6,78 @@
 
 regex_t regex;
 
-Request * make_request(char * raw_req) {
-  int reti;
-  int j;
-  int lineNum = 0;
-  size_t nmatch = 2;
+void get_matches(char * pattern, char * target, char ** matches) {
   regmatch_t* pmatch = malloc(sizeof(regex_t) * (regex.re_nsub + 1));
-  Request *r = malloc(sizeof(Request));
+  int count = 0;
 
-  regcomp(&regex, "([^\r\n]+)", REG_EXTENDED);
-
-  while (regexec(&regex, raw_req, regex.re_nsub + 1, pmatch, 0) == 0){
+  regcomp(&regex, pattern, REG_EXTENDED);
+  while (regexec(&regex, target, regex.re_nsub + 1, pmatch, 0) == 0){
     int start = pmatch[0].rm_so;
     int end = pmatch[0].rm_eo;
     int len = end - start;
     char match[len];
 
-    memcpy(match, raw_req + start, len);
+    memcpy(match, target + start, len);
     match[len] = '\0';
 
     printf("match from index %d to %d: \"%s\"\n", start, end, match);
+    // *matches[count] = match;
+    strcpy(matches[count], match);
 
-    raw_req = raw_req + end;
-    lineNum++;
+    target = target + end;
+    count++;
   }
   free(pmatch);
   regfree(&regex);
+}
+
+Request * make_request(char * raw_req) {
+  // int reti;
+  // int j;
+  // int lineNum = 0;
+  // regmatch_t* pmatch = malloc(sizeof(regex_t) * (regex.re_nsub + 1));
+  Request *r = malloc(sizeof(Request));
+
+  char * matches[103];
+  get_matches("([^\r\n]+)", raw_req, matches);
+  printf("final match: %s\n", matches[0]);
+
+
+  // regcomp(&regex, "([^\r\n]+)", REG_EXTENDED);
+
+  // while (regexec(&regex, raw_req, regex.re_nsub + 1, pmatch, 0) == 0){
+  //   int start = pmatch[0].rm_so;
+  //   int end = pmatch[0].rm_eo;
+  //   int len = end - start;
+  //   char line[len];
+
+  //   memcpy(line, raw_req + start, len);
+  //   line[len] = '\0';
+
+  //   printf("match from index %d to %d: \"%s\"\n", start, end, line);
+
+  //   if (lineNum == 0) {
+  //     //Store this line into RequestLine
+  //     // Extract each 'word' separated by spaces
+  //     regcomp(&regex, "([^ ]+)", REG_EXTENDED);
+  //     while (regexec(&regex, line, regex.re_nsub + 1, pmatch, 0) == 0){
+  //       int start = pmatch[0].rm_so;
+  //       int end = pmatch[0].rm_eo;
+  //       int len = end - start;
+  //       char match[len];
+
+  //       memcpy(match, raw_req + start, len);
+  //       match[len] = '\0';
+
+  //       printf("inner match from index %d to %d: \"%s\"\n", start, end, match);
+  //       line = line + end;
+  //     }
+  //   }
+  //   raw_req = raw_req + end;
+  //   lineNum++;
+  // }
+  // free(pmatch);
+  // regfree(&regex);
   return r;
 }
 
