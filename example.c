24008b3f5180a8e87f0f933376184aa02785fc41
Serena Chen <serenazychen@gmail.com>
2017-02-27 21:33:39 -0500

    comments on the example

--- example.c
+++ example.c
@@ -8,16 +8,32 @@
 #define QUEUE_SIZE 10
 #define PORT 8080
 
+/* Makes a response that says "Hello World"
+ *
+ * request: unused
+ *
+ * returns: A response with status code 200 and an html page that says
+ *          "Hello World"
+ */
 Response *hello_world(Request *request)
 {
     return build_response(200, "<body>Hello World!</body>\r\n");
 }
 
+/* Makes a response that says "Hello Sam"
+ *
+ * request: unused
+ *
+ * returns: A response with status code 200 and an html page that says
+ *          "Hello Sam"
+ */
 Response *hello_sam(Request *request)
 {
     return build_response(200, "<body>Hello Sam!</body>\r\n");
 }
 
+/* Makes a n application with two endpoints; runs on our server library
+ */
 int main()
 {
     Application *app = create_application(PORT, QUEUE_SIZE);
